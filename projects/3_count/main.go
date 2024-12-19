78dac3887618d7bc6743fc620ad82903375f25c0
dashadarklord2005 <dariaserezhina@gmail.com>
2024-12-19 23:19:55 +0300

    Update main.go

--- projects/3_count/main.go
+++ projects/3_count/main.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
-	// "strconv" // вдруг понадобится ;)
+	"net/http"
+	"strconv"
 )
 
+var k int
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if r.Method == "GET" {
+		fmt.Fprintf(w, "%d", k)
+	} else if r.Method == "POST" {
+		delta := r.Form.Get("count")
+		idelta, err := strconv.Atoi(delta)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "это не число")
+			return
+		}
+		k += idelta
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Метод не поддерживается")
+	}
+}
+
 func main() {
-	fmt.Println("Hi, Bro!!! 🥴")
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера", err)
+	}
 }
